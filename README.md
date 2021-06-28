dd24b111fa45f1efcd902d7de58ee8ace0cd2158
Anatolii Veresov <veresov@biocad.ru>
2021-06-28 22:16:34 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# gb-devsecops-iac2
---
+++ vm-tf/provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+credentials = "${file("gifted-cooler-221712-9aa26ac8cc3f.json")}"
+project = "gifted-cooler-221712"
+region = "us-central1"
+}
